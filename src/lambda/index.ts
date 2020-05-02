b71bb182eca38fe9a850ac2cc989a963d86c307b
PolyWTF <poly.wtf@gmail.com>
2020-05-02 17:23:28 +0900

    change api response format & fix netlify.toml

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -6,12 +6,23 @@ const takeOutQiitaTrends = (html: string) => {
   const $ = cheerio.load(html)
   const raw = $('div[data-hyperapp-app="Trend"]').attr('data-hyperapp-props')
   if (raw === undefined) return {}
-  return JSON.parse(raw).trend.edges
+  return format(JSON.parse(raw).trend.edges)
 }
 
-interface TrendResponse {
-  statusCode: number
-  body: string
+const format = (object: QiitaTrend[]) => {
+  let formattedArray = []
+  formattedArray = object.map(fetchedObject => {
+    return {
+      createdAt: fetchedObject.node.createdAt,
+      likesCount: fetchedObject.node.likesCount,
+      title: fetchedObject.node.title,
+      user: {
+        profileImageUrl: fetchedObject.node.author.profileImageUrl,
+        name: fetchedObject.node.author.urlName
+      }
+    }
+  })
+  return formattedArray
 }
 
 export const handler = (evnet: APIGatewayEvent, context: Context, callback: Callback) => {
@@ -23,4 +34,25 @@ export const handler = (evnet: APIGatewayEvent, context: Context, callback: Call
       body: JSON.stringify(takeOutQiitaTrends(response.data))
     })
   })
+}
+
+export interface QiitaTrend {
+  followingLikers: any[],
+  isLikedByViewer: boolean,
+  isNewArrival: boolean,
+  hasCodeBlock: boolean,
+  node: QiitaTrendNode
+}
+
+interface QiitaTrendNode {
+  createdAt: string
+  likesCount: number
+  title: string
+  uuid: string
+  author: QiitaTrendAuthor
+}
+
+interface QiitaTrendAuthor {
+  profileImageUrl: string
+  urlName: string
 }
